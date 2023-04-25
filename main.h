2a76b02887491ed2fc04fdb430c779d8520118b6
JOEGREEEN <yusufabdelfattah207@gmail.com>
2023-04-25 15:18:43 +0200

    Fixed merge Conflict

--- main.h
+++ main.h
@@ -9,15 +9,9 @@ char *flagger(char *flag);
 char *width(char *n);
 char *precision(char *exp);
 char *length(char *n);
-<<<<<<< HEAD
 void puts_rev(char *s);
 void _puts(char *str);
 void rot13(char *str);
 void print_number(int n);
 void numbers_printer(int n);
-=======
-int puts_rev(char *s);
-int _puts(char *str);
-int rot13(char *str);
->>>>>>> 1370b4748faeeef2577d7e7a2f472774ab1b3e59
 #endif
